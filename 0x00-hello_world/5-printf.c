72acb0a13b28b4eda689cc88efbe81923f0f314f
bakabeko <bakamoaz75@gmail.com>
2023-09-10 02:36:58 +0200

    hello,world!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{                                                 printf("with propar grammar , but the outcome is a piece of art");
+printf("\n");
+return (0);
+}
