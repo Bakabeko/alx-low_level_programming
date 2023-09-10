d5b9fb8d2d821f7abe9ca5e3d67d36e80ec3c41e
bakabeko <bakamoaz75@gmail.com>
2023-09-10 20:42:02 +0200

    good

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,19 +5,21 @@
 /**
  *main - false or true validation
  *
- *Return : 0 (sucess)
+ * description : test a randum number if it is negative or psitive or zero
+ *
+ *Return : 0 always (sucess)
 */
 
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if ( n > 0 )
-	printf("%i is positive\n", n);
-else if ( n == 0 )
-        printf("%i is zero\n", n);
-else
-        printf("%i is negative\n", n);
-return (0);
+         int n;
+         srand(time(0));
+         n = rand() - RAND_MAX / 2;
+         if ( n > 0 )
+		 printf("%i is positive\n", n);
+        else if ( n == 0 )
+		printf("%i is zero\n", n);
+        else
+		printf("%i is negative\n", n);
+        return (0);
 }
