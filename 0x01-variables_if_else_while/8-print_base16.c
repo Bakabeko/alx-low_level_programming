666d291c4d4579efa778deae209d23952c8b17b3
bakabeko <bakamoaz75@gmail.com>
2023-09-11 03:01:56 +0200

    good

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,12 @@ int main(void)
 {
 int num = 48;
 while (num <= 102) {	 
-if (num == 58) 
+if (num == 58) {
 	num += 39;
 
         putchar(num);
         num++;
+}
 }
         putchar('\n');
 return (0);
