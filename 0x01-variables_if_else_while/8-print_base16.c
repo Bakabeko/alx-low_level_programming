b6cbc78a961a63459d3446fb863fa01e0b44bb38
bakabeko <bakamoaz75@gmail.com>
2023-09-11 03:12:33 +0200

    good

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - print hexdecimal number from 0 to 9
+ * main - Entry point
  *
- * Return: always 0 (succses)
+ * Return: Always 0 (Succes)
  */
 
 int main(void)
 {
-int num = 48;
-while (num <= 102) {	 
-if (num == 58) {
-	num += 39;
+	int n;
 
-        putchar(num);
-        num++;
+	for (n = 0 ; n < 16 ; n++)
+	{
+		if (n < 10)
+		{
+			putchar('0' + n);
+		}
+		else
+		{
+			putchar(87 + n);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-}
-        putchar('\n');
-return (0);
-}
+
+
