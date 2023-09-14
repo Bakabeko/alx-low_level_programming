48d332be45c6f0b4bb91a8ea9eb87d9437f1bf3f
bakabeko <bakamoaz75@gmail.com>
2023-09-14 02:03:16 +0200

    good

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * description : check about the input is lowercase or uppercase
+ *
+ *return : 0 (success)
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+        return (0);
+}
