3596014be172e385578cddd96d4ed556e23a2e89
bakabeko <bakamoaz75@gmail.com>
2023-09-27 02:02:14 +0200

    good

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return ((s+i));
+		}
+	}
+	if (c == NULL)
+		return ((s+i);
+        return NULL;
+}
