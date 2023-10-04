4ae3edc720382519c7d1163096f9310c06dc4d87
bakabeko <bakamoaz75@gmail.com>
2023-10-05 01:35:08 +0200

    good

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - fuction copies the string pointed
+ * @dest: pointer to copy the string to.
+ * @src: the string to copy to the destination.
+ *
+ * Return: a copy of the src.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
