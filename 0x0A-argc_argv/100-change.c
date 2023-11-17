a10d078717c1d7a4cf8868d0f5c68fc5c50b2fad
Bakabeko <bakamoaz75@gmail.com>
2023-11-18 01:08:53 +0200

    good

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,76 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int give_change(int money);
-
 /**
- * main - prints the minimum number of coins to make change
- * for an amount of money
- * @argc: number of arguments.
- * @argv: amount of money given in singles.
- *
- * Return: 0 (sucesss) , 1 (Failure)
- */
+*main - prints the minimum number of coins to make change
+* for an amount of money
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 1 if there is an error; else returns 0
+*/
+
 int main(int argc, char *argv[])
 {
-	int money, change;
+int cents, coins = 0;
 
-	money = change = 0;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	money = atoi(argv[1]);
-	change = give_change(money);
-
-	printf("%d\n", change);
-	return (0);
-}
-
-/**
- * give_change - find the minimum number of coins to make change
- * for an amount of money
- * @money: amount of money given in singles.
- *
- * Return: min amount of coins. (INT)
- */
-int give_change(int money)
+if (argc != 2)
 {
-	int one, two, five, ten, tf, sum;
-
-	sum = one = two = five = ten = tf = 0;
-
-	if (money <= 0)
+	printf("Error\n");
+	return (1);
+}
+cents = atoi(argv[1]);
+while (cents > 0)
+{
+	coins++;
+	if ((cents - 25) >= 0)
 	{
-		return (0);
+		cents -= 25;
+		continue;
 	}
-
-	if (money >= 25)
+	if ((cents - 10) >= 0)
 	{
-		tf = money / 25;
-		money -= tf * 25;
+		cents -= 10;
+		continue;
 	}
-
-	if (money < 25 && money >= 10)
+	if ((cents - 5) >= 0)
 	{
-		ten = money / 10;
-		money -= ten * 10;
+		cents -= 5;
+		continue;
 	}
-
-	if (money < 10 && money >= 5)
+	if ((cents - 2) >= 0)
 	{
-		five = money / 5;
-		money -= five * 5;
+		cents -= 2;
+		continue;
 	}
-	if (money < 5 && money >= 2)
-		two = money / 2;
-		money -= two * 2;
-	if (money < 2)
-		one = money;
-
-	sum = tf + ten + five + two + one;
-	return (sum);
+	cents--;
+}
+printf("%d\n", coins);
+return (0);
 }
