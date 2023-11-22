cd17daa29c8d0595d72140d16f93d68f9226ce64
Bakabeko <bakamoaz75@gmail.com>
2023-11-22 23:41:49 +0200

    ok

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,31 +1,41 @@
 #include "main.h"
+
 /**
-*_strspn - search the number of bytes in the initial
-* segment of s which consist only of bytes from accept
-*@s:segment targeted
-*@accept:reference bytes container
-*
-*Return:returns the number of bytes in the initial
-* segment of s which consist only of bytes from accept
-*/
+ * _strspn - function  calculates  the  length (in bytes) of the initial
+ * segment of s which consists entirely of bytes in accept.
+ * @s: string to search in.
+ * @accept: bytes in which to search for.
+ *
+ * Return: the number (U_INT) of bytes matching accept.
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int i;
+	unsigned int i, z, count, checker;
 
-	while (*s)
+	i = 0;
+	z = 0;
+	checker = 0;
+	count = 0;
+	while (s[i] != '\0')
 	{
-		for (i = 0; accept[i]; i++)
+		z = 0;
+		checker = 0;
+		while (accept[z] != '\0')
 		{
-			if (accept[i] == *s)
+			if (accept[z] == s[i])
 			{
-				bytes++;
+				count++;
+				checker = 1;
+			/* Break to early increase efficiency */
 				break;
 			}
-			else if ((accept[i + 1]) == '\0')
-				return (bytes);
+			z++;
 		}
-		s++;
+
+		if (checker == 0)
+			return (count);
+		i++;
 	}
-	return (bytes);
+	return (count);
 }
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <ctype.h>
+
 /**
  * _isalpha - check if the given character is lower case.
  * @c: character to test.
  *
  * Return: 1 (True) else 0 (False)
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
